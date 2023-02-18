b7481d32e5f819b61ddd830b35bbc57aeed54200
Sultan Saparbekov <ssc001@campus.mephi.ru>
2023-02-18 14:10:58 +0300

    annotation @Component

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,27 +8,10 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+        Music rockMusic = context.getBean("rockMusic", Music.class);
 
-        System.out.println(classicalMusic.getSong());
+        System.out.println(rockMusic.getSong());
 
-//        System.out.println(classicalMusic1.getSong());
-
-
-//        Music music = context.getBean("musicBean", Music.class);
-//
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-
-//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-//
-//        musicPlayer1.playMusic();
-//
-//        System.out.println(musicPlayer1.getName());
-//        System.out.println(musicPlayer1.getVolume());
-//
-//        // scope prototype (false), singleton (true)
-//        System.out.println(musicPlayer1 == musicPlayer2);
         context.close();
     }
 }
