c58f9840ac4a3c4eda5d538642cdab450e449cca
Sultan Saparbekov <ssc001@campus.mephi.ru>
2023-02-18 13:37:55 +0300

    init, destroy methods

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package org.example;
 
 public class ClassicalMusic implements Music{
+    public void doMyInit() {
+        System.out.println("Doing my init");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destroy");
+    }
+
     @Override
     public String getSong() {
         return "classic song";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,19 +8,24 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+
 //        Music music = context.getBean("musicBean", Music.class);
 //
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-
-        musicPlayer1.playMusic();
-
-        System.out.println(musicPlayer1.getName());
-        System.out.println(musicPlayer1.getVolume());
-
-        System.out.println(musicPlayer1 == musicPlayer2);
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        musicPlayer1.playMusic();
+//
+//        System.out.println(musicPlayer1.getName());
+//        System.out.println(musicPlayer1.getVolume());
+//
+//        // scope prototype (false), singleton (true)
+//        System.out.println(musicPlayer1 == musicPlayer2);
         context.close();
     }
 }
